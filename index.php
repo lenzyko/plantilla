dace28a419b3a1fc87e39005a50db51e37382cb6
lenzyko <31932199+lenzyko@users.noreply.github.com>
2017-09-20 16:24:20 -0500

    escribiendo el p
    poniendo varias etiquetas

--- index.php
+++ index.php
@@ -6,5 +6,7 @@
 </head>
 <body>
 	<h1>cripto ínca</h1>
+	<h2>subtítulo indicando todo</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur fuga, maiores rem in consequatur sequi optio, cum eos consequuntur reiciendis, omnis suscipit veritatis nulla quos explicabo maxime autem odit coris.</p>
 </body>
 </html>
