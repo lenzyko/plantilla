342c75fbc8b6c1d5b3cf281f471875003cb90b19
lenzyko <31932199+lenzyko@users.noreply.github.com>
2017-09-21 12:59:13 -0500

    agregar

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 	<h1>cripto ínca</h1>
 	<h2>subtítulo indicando todo</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur fuga, maiores rem in consequatur sequi optio, cum eos consequuntur reiciendis, omnis suscipit veritatis nulla quos explicabo maxime autem odit coris.</p>
+	<section>kasuf</section>
 </body>
 </html>
